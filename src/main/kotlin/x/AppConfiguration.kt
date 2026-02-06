a0a8db0bfd37fa5fb7d4f60cb877f64e60cfdcc8
Pavel Taruts <ptrts@mail.ru>
2026-02-07 02:15:44 +0300

    Переводим парсинг полученного из топика объекта с Deserializer на MessageConverter

--- src/main/kotlin/x/AppConfiguration.kt
+++ src/main/kotlin/x/AppConfiguration.kt
@@ -4,9 +4,14 @@ import org.apache.kafka.clients.admin.NewTopic
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.support.converter.MessageConverter
+import org.springframework.kafka.support.converter.StringJacksonJsonMessageConverter
 
 @Configuration
 class AppConfiguration {
+
+    @Bean
+    fun messageConverter(): MessageConverter = StringJacksonJsonMessageConverter()
 
     @Bean
     fun topicConfig(
